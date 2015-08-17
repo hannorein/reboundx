71d6bd72861ef67029bcc8d87f20468709f7cdb9
Daniel Tamayo <tamayo.daniel@gmail.com>
2015-08-17 10:18:25 -0400

    latest changes

--- cext/libreboundxf.c
+++ cext/libreboundxf.c
@@ -230,7 +230,41 @@ void rebxf_modify_elements(struct reb_simulation* const sim){
 
 		com = xftools_get_com_of_pair(com, sim->particles[i]);
 	}
+	//xftools_move_to_com(sim->particles, sim->N);
+}
+
+void rebxf_modify_elements_test(struct reb_simulation* const sim){
+	struct rebxf_params* xf = (struct rebxf_params*)sim->xf_params;
+	struct reb_particle com = xftools_get_com(sim);//sim->particles[0];
+	for(int i=1;i<sim->N;i++){
+		struct reb_particle *p = &(sim->particles[i]);
+		struct reb_orbit o = xftools_p2orbit(sim->G, sim->particles[i], com);
+	    double da = 0.;
+		double de = 0.;
+		double dpo = 0.;	
+		if (xf->tau_a[i] != 0.){
+			da += -o.a*sim->dt/xf->tau_a[i]; 
+		}
+		
+		if (xf->tau_e[i] != 0.){
+			de += -o.e*sim->dt/xf->tau_e[i];
+			da += -2.*o.a*o.e*o.e*xf->e_damping_p*sim->dt/xf->tau_e[i];
+		}
+
+		if (xf->tau_pomega[i] != 0.){
+			dpo += 2*M_PI*sim->dt/xf->tau_pomega[i];
+		}
+
+		o.a += da;
+		o.e += de;
+		o.omega += dpo;
+
+		xftools_orbit2p(&sim->particles[i], sim->G, &com, o); 
+
+		//com = xftools_get_com_of_pair(com, sim->particles[i]);
+	}
 	xftools_move_to_com(sim->particles, sim->N);
 }
+
 
 
